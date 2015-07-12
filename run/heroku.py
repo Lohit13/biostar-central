ac15bfa5029cd7896585700e03ff074d78995632
Lohit <lohitaksh14059@iiitd.ac.in>
2015-07-13 02:43:34 +0530

    Added heroku.py. gitignore was excluding it

---
+++ run/heroku.py
@@ -0,0 +1,32 @@
+# Import all values from the base then override site specific settings.
+from biostar3.settings.base import *
+
+# Turn this off during deployment.
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+# Site administrators. Make sure to override this.
+ADMINS = (
+    (get_env("BIOSTAR_ADMIN_NAME"), get_env("BIOSTAR_ADMIN_EMAIL")),
+)
+
+DEFAULT_FROM_EMAIL = get_env("DEFAULT_FROM_EMAIL")
+
+MANAGERS = ADMINS
+
+# Needs to match the server domain.
+SESSION_COOKIE_DOMAIN = ".lvh.me"
+
+# This must be set correctly in production.
+ALLOWED_HOSTS = ["localhost", get_env("BIOSTAR_HOSTNAME"), ".lvh.me"]
+
+# The secret key can be used to log into the admin account!
+# Make sure to change it in production.
+SECRET_KEY = get_env("SECRET_KEY")
+
+# The database name must be present.
+DATABASE_NAME = get_env('DATABASE_NAME')
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
